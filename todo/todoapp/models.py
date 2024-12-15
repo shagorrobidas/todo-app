7e9f2bbdf7b2beba274f1b0b275228a286e1a7da
Shagor Robidas <roysagor88@gmail.com>
2024-12-15 20:30:55 +0600

    create model Todo

--- todo/todoapp/models.py
+++ todo/todoapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
