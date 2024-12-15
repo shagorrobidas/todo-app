1225be74ff3e39395866c7fff9648b6578901576
Shagor Robidas <roysagor88@gmail.com>
2024-12-15 23:53:41 +0600

    add delete method

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -5,5 +5,7 @@ from  . import views
 urlpatterns = [
     path('', views.home, name="home-page"),
     path('register/', views.register, name="register"),
-    path('login/', views.loginpage, name="loginpage")
+    path('login/', views.loginpage, name="loginpage"),
+    path('delete-tesk/<str:name>/', views.DeleteTask, name="delete"),
+    path('update-tesk/<str:name>/', views.UpdateTaks, name="update"),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -6,16 +6,20 @@ from .models import Todo
 
 # Create your views here.
 
-
 def home(request):
     if request.method == 'POST':
         task = request.POST.get('task')
+        if task:  # Ensure task is not empty
+            newTodo = Todo(user=request.user, name=task)
+            newTodo.save()
 
-        newTodo = Todo(user=request.user, name=task)
-        print(newTodo)
-        newTodo.save()
+    # Fetch all todos for both GET and POST requests
+    all_todos = Todo.objects.filter(user=request.user)
+    context = {
+        'todos': all_todos
+    }
 
-    return render(request, 'todoapp/todo.html', {})
+    return render(request, 'todoapp/todo.html', context)
 
 
 def register(request):
@@ -61,3 +65,13 @@ def loginpage(request):
             return redirect('loginpage')
 
     return render(request, 'todoapp/login.html', {})
+
+
+def DeleteTask(request, name):
+    get_todo = Todo.objects.get(user=request.user, name=name)
+    get_todo.delete()
+    return redirect('home-page')
+
+
+def UpdateTaks(request, name):
+    pass
