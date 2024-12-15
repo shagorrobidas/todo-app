3e8b50ee3421c6a0e0e038e9b0086901ae973131
Shagor Robidas <roysagor88@gmail.com>
2024-12-15 19:05:34 +0600

    add registerpage data

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout 
 
 # Create your views here.
 
@@ -8,6 +10,14 @@ def home(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        newUser = User.objects.create_user(
+            username=username, email=email, password=password)
+        newUser.save()
     return render(request, 'todoapp/register.html', {})
 
 
